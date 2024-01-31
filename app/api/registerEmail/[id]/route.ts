a2cc9810342f27a4dbdee2b5bcaa22a0008c3c22
felix-nn123 <upwork_fn@yahoo.com>
2024-01-31 17:37:59 +0100

    some changes again

--- app/api/registerEmail/[id]/route.ts
+++ app/api/registerEmail/[id]/route.ts
@@ -1,19 +1,24 @@
 import connectionDB from "@/libs/mongodb";
 import registerEmail from "@/models/registerEmail";
+import { useRouter } from "next/router";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PUT(request: NextRequest, { params: any }) {
+export async function PUT(request: NextRequest) {
+  const router = useRouter();
+  const { id } = router.query;
   const { username, email } = await request.json();
   await connectionDB();
-  const user = await registerEmail.findByIdAndUpdate(params.id, {
+  const user = await registerEmail.findByIdAndUpdate(id, {
     username,
     email,
   });
   return NextResponse.json({ data: user }, { status: 200 });
 }
 
-export async function GET(request: Request, { params: any }) {
+export async function GET(request: Request) {
+  const router = useRouter();
+  const { id } = router.query;
   await connectionDB();
-  const user = await registerEmail.findById(params.id);
+  const user = await registerEmail.findById(id);
   return NextResponse.json({ data: user }, { status: 200 });
 }
