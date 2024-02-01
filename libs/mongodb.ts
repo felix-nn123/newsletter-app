7d94daf54bfb793ec88e2caaa7619eff52f030d7
felix-nn123 <upwork_fn@yahoo.com>
2024-02-01 12:53:06 +0100

    some dull typescript

--- libs/mongodb.ts
+++ libs/mongodb.ts
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 
-const printEnv = (value: string ) => {
-  console.log(value)
-  return value
+interface processParams{
+  env:{
+    MONGO_URI:string
+  }
+}
+
+const printEnv = (process: processParams ) => {
+  return process.env.MONGO_URI
 };
 
 const connectionDB = async () => {
   try {
-    await mongoose.connect(printEnv(process?.env?.MONGO_URI));
+    await mongoose.connect(printEnv(process));
     console.log("Connected to database");
   } catch (error) {
     console.log("Error connecting to database", error?.message);
