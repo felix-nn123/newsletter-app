88f1145648df8299e229c504ca62d5e3571ee876
felix-nn123 <upwork_fn@yahoo.com>
2024-02-01 12:27:57 +0100

    some changes

--- libs/mongodb.ts
+++ libs/mongodb.ts
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
+const printEnv = (value: string | undefined) => {
+  return value
+};
+
 const connectionDB = async () => {
   try {
-    await mongoose.connect(process?.env?.MONGO_URI);
+    await mongoose.connect(printEnv(process?.env?.MONGO_URI));
     console.log("Connected to database");
   } catch (error) {
     console.log("Error connecting to database", error?.message);
