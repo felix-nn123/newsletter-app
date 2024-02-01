f5e9d29c6f389c71a947e131eb688801f323ed9a
felix-nn123 <upwork_fn@yahoo.com>
2024-02-01 13:02:37 +0100

    some dull error

--- libs/mongodb.ts
+++ libs/mongodb.ts
@@ -1,18 +1,10 @@
 import mongoose from "mongoose";
 
-interface processParams{
-  env:{
-    MONGO_URI:string
-  }
-}
-
-const printEnv:string = (process: processParams ) => {
-  return process.env.MONGO_URI
-};
+const MONGO_URI: string = (process.env.MONGO_URI as string);
 
 const connectionDB = async () => {
   try {
-    await mongoose.connect(printEnv(process));
+    await mongoose.connect(MONGO_URI);
     console.log("Connected to database");
   } catch (error) {
     console.log("Error connecting to database", error?.message);
