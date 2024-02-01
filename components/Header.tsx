6f067ba0a582e38d5e13d0ede57fa2f398012b63
felix-nn123 <upwork_fn@yahoo.com>
2024-02-01 23:54:10 +0100

    some changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,21 +31,21 @@ const Header = ({
 }: Props) => {
   return (
     <div className="w-full h-full bg-slate-100">
-      <div className="flex lg:flex-row md:flex-col w-full h-full overflow-hidden">
+      <div className="flex  flex-col lg:flex-row w-full h-full overflow-hidden">
         <div className="flex-1 relative bg-text-black/50">
           <img
             className="absolute w-full h-full"
             src="images/news-background.jpg"
           />
           <div
-            className={`flex flex-col absolute z-5 w-full h-full justify-center items-center p-4 bg-[rgba(0,0,0,0.9)]}`}
+            className={`flex flex-col absolute z-5 w-full h-full justify-center items-center p-4 bg-[rgba(0,0,0,0.7)]`}
           >
             <h3
-              className={`text-6xl text-center text-white font-bold leading-relaxed ${lobster.className}`}
+              className={`text-3xl text-center text-white font-bold leading-10 md:leading-relaxed md:text-6xl ${lobster.className}`}
             >
               Welcome to our news app
             </h3>
-            <h3 className="text-6xl text-center font-bold leading-relaxed text-blue-500">
+            <h3 className="text-3xl text-center font-bold leading-10 md:leading-relaxed md:text-6xl text-blue-500">
               Please subscribe to our Newsletter
             </h3>
           </div>
@@ -67,7 +67,7 @@ const Header = ({
               ""
             )}
             <TextField
-              className="w-2/3"
+              className="w-5/6 md:w-2/3"
               id="standard-basic"
               label="Full Name"
               variant="standard"
@@ -75,7 +75,7 @@ const Header = ({
               required
             />
             <TextField
-              className="w-2/3"
+              className="w-5/6 md:w-2/3"
               id="standard-basic"
               label="Please enter your email address"
               variant="standard"
@@ -86,7 +86,7 @@ const Header = ({
 
             <button
               onClick={(e: React.MouseEvent) => onClickSubmit(e)}
-              className="w-1/3 mt-10 bg-gray-800 text-white p-3 rounded-3xl hover:bg-gray-100 hover:text-black"
+              className="w-4/5 md:w-1/3 mt-10 bg-gray-800 text-white p-3 rounded-3xl hover:bg-gray-100 hover:text-black"
               type="submit"
               disabled={loading}
             >
